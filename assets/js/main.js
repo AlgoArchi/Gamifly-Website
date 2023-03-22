677cd4ff84a83d99d5534d5ed5754233b82999d4
faiezharis1230 <faiezharis1230@gmail.com>
2023-03-22 17:00:04 +0630

    add policy

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,42 @@ window.addEventListener("scroll",() => {
         document.querySelector("header").classList.remove("d-none")
     }
 })
+
+
+jQuery(document).ready(function($) {
+    $('.slick.marquee').slick({
+      speed: 5000,
+      autoplay: true,
+      autoplaySpeed: 0,
+      centerMode: true,
+      cssEase: 'linear',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      infinite: true,
+      initialSlide: 1,
+      arrows: false,
+      buttons: false
+    });
+  });
+
+  jQuery(document).ready(function($) {
+    $('.slick.member').slick({
+      speed: 500,
+    //   autoplay: false,
+    //   autoplaySpeed: 0,
+      centerMode: true,
+    //   cssEase: 'linear',
+      slidesToShow: 4,
+      slidesToScroll: 1,
+    //   variableWidth: true,
+    //   infinite: true,
+      initialSlide: 1,
+    //   asNavFor: '.slider-for',
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      arrows: false,
+    //   buttons: false
+    });
+  });
