e9bfeb8b6f26b1ff22b06e8ca6107b1627c54132
faiezharis1230 <faiezharis1230@gmail.com>
2023-05-24 16:12:16 +0630

    update web

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,71 +9,19 @@ window.addEventListener("scroll",() => {
     }
 })
 
+let faqLists = document.querySelectorAll("#blog .list .list-item")
+let mediumLink = document.querySelector("#medium-link-btn")
 
-jQuery(document).ready(function($) {
-    $('.slick.marquee').slick({
-      speed: 5000,
-      autoplay: true,
-      autoplaySpeed: 0,
-      centerMode: true,
-      cssEase: 'linear',
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      variableWidth: true,
-      infinite: true,
-      initialSlide: 1,
-      arrows: false,
-      buttons: false
-    });
-  });
-
-  jQuery(document).ready(function($) {
-    $('.slick.member').slick({
-      speed: 500,
-    //   autoplay: false,
-    //   autoplaySpeed: 0,
-      centerMode: true,
-    //   cssEase: 'linear',
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-          },
-        },
-        {
-          breakpoint: 1008,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          },
-        },
-        {
-            breakpoint: 800,
-            settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            },
-        },
-        {
-            breakpoint: 600,
-            settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            },
-        },
-      ],    
-    //   variableWidth: true,
-    //   infinite: true,
-      initialSlide: 1,
-    //   asNavFor: '.slider-for',
-      dots: false,
-      centerMode: true,
-      focusOnSelect: true,
-      arrows: false,
-    //   buttons: false
-    });
-  });
+Array.from(faqLists).map((item,index) => {
+  item.addEventListener('click',li => {
+    Array.from(faqLists).map(i => {
+      i.style.backgroundColor = "#151515"
+      i.firstElementChild.nextElementSibling.style.display = 'none'
+      i.firstElementChild.firstElementChild.nextElementSibling.style.display = 'none'
+    })
+    li.target.style.backgroundColor = "#232323"
+    li.target.firstElementChild.nextElementSibling.style.display = 'block'
+    li.target.firstElementChild.firstElementChild.nextElementSibling.style.display = 'block'
+    mediumLink.href = li.target.id
+  })
+})
